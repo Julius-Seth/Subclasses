5ef2970b0fd367fc59e0217153fc77e37c6ff619
unknown <juliusahianu@gmail.com>
2019-06-19 23:51:55 -0700

    Subclasses

---
+++ Room.java
@@ -0,0 +1,81 @@
+class Room{
+
+  private double width;
+ private double length;
+ private int floor;
+
+
+public class Room(){
+double width=10;
+double length=12.5;
+int floorwidth=1;
+
+}
+
+public double getWidth(){
+	return width;
+	
+}
+
+public void setWidth(double width){
+	this.width= width;
+	
+	
+
+}
+public double getLength(){
+	return length;
+	
+}
+
+public void setLength(double length){
+	this.length= length;
+   
+}
+public int getFloor(){
+	return floor;
+	
+}
+
+public void setFloor(int floor){
+	this.floor= floor;
+}
+
+public void setWidth(double width){
+	if(width=0||-10){
+		this.width=width;
+
+	}
+
+	public void setLength(double length){
+	if(length=0||-10){
+		this.length=length;
+	
+	}
+}
+}
+public class Room(double length,double width, int floor){
+
+	this.length=length;
+	this.width=width;
+	this.floor=floor;
+}
+public class Room{
+	Static void toString(){
+		System.out.println("length * width on floor floor");
+	}
+	public static void main(String [] args){
+		toString();
+	}
+}
+class Classroom{
+	private int  numStudents= 50;
+}
+class Classroom(){
+	double length;
+	double width;
+	int floor;
+
+	int numStudents
+}
+}
